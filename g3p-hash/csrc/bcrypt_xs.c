ff4d74a8d7de7d9744b92b3aafbcf4149fdf2323
leon.smith@iterative.systems <leon.smith@iterative.systems>
2024-04-25 12:47:18 -0400

    Reduce minimum number of bcrypt expansions in xs-ctr mode

--- g3p-hash/csrc/bcrypt_xs.c
+++ g3p-hash/csrc/bcrypt_xs.c
@@ -92,11 +92,6 @@ bcrypt_xs_ctr_expand
   /* Written so that things work when rounds == UINT32_MAX */
   rounds++;
   tagPos = 0;
-  G3P_Blowfish_expandCtr
-    (state, key0, key0Len, name, nameLen, tag, tagLen, &tagPos, rounds, true);
-  tagPos = 0;
-  G3P_Blowfish_expandCtr
-    (state, key1, key1Len, name, nameLen, tag, tagLen, &tagPos, ~rounds, true);
   uint32_t roundPos;
   do {
     rounds--;
